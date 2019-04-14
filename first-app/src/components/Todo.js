4909ffb41969e561d13b470f6c607d1d24bc6ce9
Turtleman1024 <kmurry6262@gmail.com>
2019-04-13 21:21:51 -0700

    Passing each Todo item to TodoItem component to render

--- first-app/src/components/Todo.js
+++ first-app/src/components/Todo.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import TodoItem from './TodoItem';
 
 /*This is the main App Component*/
 class Todo extends Component {
   render() {
     return this.props.todos.map((todos) => (
-        <h3>{ todos.title}</h3>
+        /*Passing the current todos array item to TodoItem props*/
+        <TodoItem todos={todos}/>
     ));
   }
 }
