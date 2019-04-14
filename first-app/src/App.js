4202229671582e6da76371fe66772fc57b75103f
Turtleman1024 <kmurry6262@gmail.com>
2019-04-13 22:35:12 -0700

    Finally found where the extra > was coming from.

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 /*npm run start */
 /*This is the main App Component*/
+/*https://www.youtube.com/watch?v=sBws8MSXN7A&t=810s
+  stopped at 49.55
+*/
 class App extends Component {
   state = {
     todos: [
@@ -39,7 +42,7 @@ class App extends Component {
       <div className="App">
       {/*Embedding the Todo component*/}
       {/*Passing todos as a prop to the todo component*/}
-        <Todo todos={this.state.todos} markComplete={this.markComplete}/>>
+        <Todo todos={this.state.todos} markComplete={this.markComplete}/>
       </div>
     );
   }
