1c160af9acccecca41a772c3d01c15a4d399484f
Turtleman1024 <kmurry6262@gmail.com>
2019-04-13 20:57:34 -0700

    Creating state with an array of a couple todos.

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -4,6 +4,25 @@ import './App.css';
 
 /*This is the main App Component*/
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Learn React',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Learn Redux',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Learn Sages',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
